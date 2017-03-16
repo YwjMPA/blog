dff7d3e549ed2ff35429d29f2052789b05426242
Ywj <somnus.yuwenjia@foxmail.com>
2017-03-17 02:19:45 +0800

    encapsulate event.js

--- js/event.js
+++ js/event.js
@@ -1,26 +1,27 @@
 // event code
+(function(){
+  let logBtn = document.querySelector('#logIn');
+  let logModal = document.querySelector('#modal');
+  let span = document.getElementsByClassName("close")[0];
 
-let logIn = document.getElementById('logIn');
-let modal = document.getElementById('modal');
-let span = document.getElementsByClassName("close")[0];
+  logBtn.addEventListener('click', function() {
+      logModal.style.display = 'block';
+    }
+  );
 
-logIn.addEventListener('click', function() {
-    modal.style.display = 'block';
-  }
-);
-
-logIn.addEventListener('touch', function() {
-    modal.style.display = 'block';
-  }
-);
+  logBtn.addEventListener('touch', function() {
+      logModal.style.display = 'block';
+    }
+  );
 
 
-span.onclick = function() {
-  modal.style.display = 'none';
-};
+  span.onclick = function() {
+    logModal.style.display = 'none';
+  };
 
-window.onclick = function(e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+  window.onclick = function(e) {
+    if (e.target == logModal) {
+      logModal.style.display = 'none';
+    }
+  };
+})();
